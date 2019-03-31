29f1e6f3fe58f7e16143b1b8494fc7faa1592014
Matheus Henrique <matheus.181137@gmail.com>
2019-03-31 16:13:04 -0400

    form post done

---
+++ front/api.js
@@ -0,0 +1,5 @@
+const api = axios.create({
+  baseURL: "http://localhost:8090/"
+});
+
+export default api;
--- front/script.js
+++ front/script.js
@@ -1,19 +1,41 @@
+class Pokemon {
+  constructor(nome, cp, hp, doce, tipo) {
+    this.nome = nome;
+    this.cp = cp;
+    this.hp = hp;
+    this.doce = doce;
+    this.tipo = tipo;
+  }
+}
+
+const nome = document.querySelector("#nome");
+const cp = document.querySelector("#cp");
+const hp = document.querySelector("#hp");
+const doce = document.querySelector("#doce");
+const tipo = document.querySelector("#tipo");
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", () => {
+  const poke = new Pokemon(
+    nome.value,
+    cp.value,
+    hp.value,
+    doce.value,
+    tipo.value
+  );
+  postUser(poke);
+});
+
 async function getUser() {
   try {
-    return await axios.get("http://localhost:8090/clientes");
+    return await axios.get("http://localhost:8090/pokemons");
   } catch (error) {
     console.error(error);
   }
 }
 
-async function postUser() {
+async function postUser(poke) {
   try {
-    return await axios.post("http://localhost:8090/clientes", {
-      nome:"joao sampaio",
-      idade: 90,
-      endereco: "endereco do joao",
-      cidade: "cidade do joao"
-    });
+    return await axios.post("http://localhost:8090/pokemons", poke);
   } catch (error) {
     console.error(error);
   }
