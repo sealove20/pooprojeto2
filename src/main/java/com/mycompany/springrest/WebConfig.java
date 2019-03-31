8c466ec63c17d622a6af35e459cf10ed6cea13d2
Matheus Henrique <matheus.181137@gmail.com>
2019-03-31 01:08:00 -0400

    CORS fixed

---
+++ src/main/java/com/mycompany/springrest/WebConfig.java
@@ -0,0 +1,13 @@
+package com.mycompany.springrest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
